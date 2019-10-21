14dfd21ddc144ba4e7d6c3ae44c68587d317f6f9
GROOT Thibault <groott@hostname.iut2.upmf-grenoble.fr>
2019-10-21 11:06:58 +0200

    ajout de color et origin

--- data/create.sql
+++ data/create.sql
@@ -1,19 +1,19 @@
 CREATE TABLE Cart(
-  ref INTEGER,
-  refProduct INTEGER FOREIGN KEY REFERENCES Products(ref),
-  quantity INTEGER
+    ref INTEGER,
+    refProduct INTEGER FOREIGN KEY REFERENCES Products(ref),
+    quantity INTEGER
 );
 CREATE TABLE Users(
-  username STRING,
-  password STRING,
-  myCart INTEGER FOREIGN KEY REFERENCES Cart(ref)
+    username STRING,
+    password STRING,
+    myCart INTEGER FOREIGN KEY REFERENCES Cart(ref)
 );
 CREATE TABLE Products(
-  ref INTEGER PRIMARY KEY,
-  title TEXT,
-  img TEXT,
-  description TEXT,
-  prix REAL,
-  color STRING,
-  origin STRING
+    ref INTEGER PRIMARY KEY,
+    title TEXT,
+    img TEXT,
+    description TEXT,
+    prix REAL,
+    color STRING,
+    origin STRING
 );
