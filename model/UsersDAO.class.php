8cf243c78602b77b8a35397de1a1ed3a3fc7784d
mermetcl <50235962+mermetcl@users.noreply.github.com>
2019-10-29 18:26:41 +0100

    récupération du panier d'un utilisateur
    ajout de la récupération du panier d'un utilisateur dans la base de données lors de l'appel de la fonction get avec en paramètre un username

--- model/UsersDAO.class.php
+++ model/UsersDAO.class.php
@@ -16,8 +16,13 @@
             //prepare (puis execute) au lieu de query
             $res = $this->db->query("SELECT * FROM Users WHERE username='$username'");
             $Users = $res->fetchAll(PDO::FETCH_CLASS,"Users");
+            $User = $Users[0];
             //récupération panier
-            return $Users[0];
+            // A TESTER //
+            $res = $this->db->query("SELECT * FROM CartItem WHERE username='$username'"); // $res prends la valeur du contenu du panier de l'utilisateur
+            $User->myCart = $res->fetchAll(PDO::FETCH_CLASS,"CartItem"); // fetchAll transforme $res en tableau, et ce tableau est stocké dans l'attribut myCart
+            //          //
+            return $User;
         }
         function getCartItem(int $refProduct, string $username):CartItem{
             $res = $this->db->query("SELECT * FROM CartItem WHERE username='$username'AND refProduct='$refProduct'");
