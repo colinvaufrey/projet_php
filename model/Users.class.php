564b2218afae5c7e1bbc5c36e8388bd7663141f3
mermetcl <50235962+mermetcl@users.noreply.github.com>
2019-10-30 10:44:47 +0100

    delete ligne inutile

--- model/Users.class.php
+++ model/Users.class.php
@@ -6,7 +6,6 @@
         function __construct($username,$password){
             $this->username = $username;
             $this->password = $password;
-            $this->myCart = 
         }
     }
 ?>
