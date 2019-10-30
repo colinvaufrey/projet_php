ee2f7cb71181870350df9d24006c7330f0bd0015
mermet <cloe.mermet@gmail.com>
2019-10-30 11:58:17 +0100

    suppression d'une mention à CartItem.class.php (qui a été supprimée)

--- model/all.test.php
+++ model/all.test.php
@@ -3,18 +3,13 @@
     require_once('ProductsDAO.class.php');
     require_once('Users.class.php');
     require_once('UsersDAO.class.php');
-    require_once('CartItem.class.php');
-    require_once('CartItemDAO.class.php');
 
     // Creation des instances DAO
     $product = new ProductsDAO();
     $user = new UsersDAO();
-    $cartItem = new CartItemDAO();
 
     $p = $product->get(1);
     var_dump($p);
-    $u = $user->get(1);
+    $u = $user->get("username");
     var_dump($u);
-    $c = $cartItem->get(1);
-    var_dump($c);
 ?>
