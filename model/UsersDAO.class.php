20a9ef0f4648c9e3f03b480bde75a4b162f08422
mermet <cloe.mermet@gmail.com>
2019-10-30 11:53:05 +0100

    rectification d'un copier/coller foireux

--- model/UsersDAO.class.php
+++ model/UsersDAO.class.php
@@ -84,7 +84,7 @@
 
                 // A TESTER //
                 /* Exécute une requête préparée en en liant des variables PHP */
-                $sql = 'DELETE FROM CartItem WHERE username='$username'AND refProduct='$refProduct''; // requête (double ou simple quotes ?)
+                $sql = 'DELETE FROM CartItem WHERE username='?'AND refProduct='?''; // requête (double ou simple quotes ?)
                 $sth = $this->db->prepare($sql); // début de la préparation
                 $sth->bindParam(1, $username, PDO::PARAM_STR, 20); // sécurisation du paramètre 1 attendu (ici une string de 20 caractères)
                 $sth->bindParam(2, $refProduct, PDO::PARAM_INT); // sécurisation du paramètre 2 attendu (ici un int)
