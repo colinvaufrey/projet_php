db1382ad8cfb43dea0db35f4a8a2ffac001738b5
mermet <cloe.mermet@gmail.com>
2019-11-13 14:21:53 +0100

    ajout du ctrl et de la vue product

---
+++ controler/product.ctrl.php
@@ -0,0 +1,28 @@
+<?php
+    // Partie principale
+
+    // Inclusion du framework
+    include_once("../framework/View.class.php");
+
+    // Inclusion du modèle
+    include_once("../model/Products.class.php");
+    include_once("../model/ProductsDAO.class.php");
+
+    // Creation de l'unique objet DAO
+    $dao = new ProductsDAO();
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Construction de la vue
+    ////////////////////////////////////////////////////////////////////////////
+    $view = new View();
+    
+    // récupérer $ref
+    $ref = $_GET["ref"];
+    $produit = $dao->getProduct($ref);
+
+    // Passe les paramètres à la vue
+    $view->assign('produit', $produit);
+
+    // Charge la vue
+    $view->display("product.view.php");
+?>
---
+++ view/product.view.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../view/design/style.css">
+        <link rel="icon" href="../view/design/images/favicon.png">
+        <title>ÊtreFruits</title>
+    </head>
+    <body>
+        <?php include("menu.view.php") ?>
+
+        <article class="main">
+            <section>
+                <article class="image">
+                    <img src="<?= $produit->img ?>" alt="Image de <?= $prod->title ?>">
+                </article>
+                <article class="titre, description, caractéristiques">
+                    <h2><?= $produit->title ?></h2>
+                    <p><?= $produit->decription ?></p>
+                    <p>
+                    Couleur : <?= $produit->color ?><br>
+                    Origine : <?= $produit->origin ?><br>
+                    Stock : <?= $produit->stock ?><br>
+                    </p>
+                </article>
+                <article class="prix et bouton"><h3>
+                    <?= $produit->prix ?>€</h3>
+                    <a href="">Ajouter au Panier</a>
+                </article>
+            </section>
+        </article>
+
+        <?php include("footer.view.php") ?>
+    </body>
+</html>
