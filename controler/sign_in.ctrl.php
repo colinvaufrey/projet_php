fd0eb9250e21f6d228a1b1186f704f30278b592d
TheKiwy <tttthekiwy@gmail.com>
2019-11-13 15:50:53 +0100

    Ajout de la page de création de compte //Ajouter à la BDD //Ajouter un lien

---
+++ controler/sign_in.ctrl.php
@@ -0,0 +1,56 @@
+<?php
+    // Partie principale
+
+    // Inclusion du framework
+    include_once("../framework/View.class.php");
+
+    // Inclusion du modèle
+    include_once("../model/CartItem.class.php");
+
+    include_once("../model/Users.class.php");
+    include_once("../model/UsersDAO.class.php");
+
+    session_start();
+
+    // Creation de l'unique objet DAO
+    $dao = new UsersDAO();
+    $isLogged = false;
+    $error = "";
+
+    if (isset($_POST['id']) && isset($_POST['pw']) && isset($_POST['pwc'])) {
+        $name = $_POST['id'];
+        $pass = $_POST['pw'];
+        $passConf = $_POST['pwc'];
+
+        $account = $dao->get($name);
+        if (!$account) {
+            if ($pass == $passConf) {
+                $user = new Users;
+                $user->username = $name;
+                $user->password = $pass;
+                //$dao->addAccount($user);
+                $_SESSION["user"] = $user;
+                $isLogged = true;
+            } else {
+                $error = "Les deux mots de passe entrés étaient différents, veuillez réessayer";
+            }
+        } else {
+            $error = "Le nom d'utilisateur entré existe déjà, veuillez réessayer avec un autre identifiant";
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Construction de la vue
+    ////////////////////////////////////////////////////////////////////////////
+    $view = new View();
+
+    // Passe les paramètres à la vue
+    $view->assign("error", $error);
+
+    // Charge la vue
+    if ($isLogged) {
+        header("Location: ../controler/main.ctrl.php");
+    } else {
+        $view->display("sign_in.view.php");
+    }
+?>
