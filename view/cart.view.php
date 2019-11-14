4eaf16ec52dfce0afc98639d453b0171a2f895ca
TheKiwy <tttthekiwy@gmail.com>
2019-11-14 16:10:53 +0100

    Dernier commit au niveau fonctionnalités ?...

--- view/cart.view.php
+++ view/cart.view.php
@@ -9,7 +9,7 @@
     <body>
         <?php include("menu.view.php");
         if (!$products){
-            echo "<article>Une erreur est survenue, veuillez retourner sur la page principale et réessayer</article>";
+            echo "<article>Votre panier est vide, revenez quand vous aurez choisi vos délicieux mets !</article>";
         } else {
         ?>
         <article class="Panier">
@@ -19,9 +19,11 @@
             public $quantity;
             -->
             <?php
+            $total = 0;
                 for($i = 0; $i < count($products); $i++) {
                     $quantity = $quantities[$i];
                     $p = $products[$i];
+                    $total += ($p->prix * $quantity);
             ?>
 
             <section>
@@ -39,23 +41,15 @@
             </section>
             <?php } ?>
 
+            <section>
+                <p>Total des prix : <?= number_format($total, 2)?> €</p>
+                <form class="" action="index.html" method="post">
+                    <input type="submit" name="submit" value="Valider et passer au paiement">
+                </form>
+            </section>
+
         </article>
 
-        <?php
-            if (count($products) == 0) {
-                echo "<article>Votre panier est actuellement vide, si c'est une erreur, veuillez réessayer de le remplir</article>";
-            }
-        ?>
-
         <?php } include("footer.view.php") ?>
-
-        <script type="text/javascript">
-
-            function updateLink(ref, quantity) {
-                let linkToEdit = document.getElementById(ref + "_updateCartLink");
-                let newURL = "../controler/add_to_cart.ctrl.php?force_update=true&ref=" + ref + "&quantity=" + quantity;
-                linkToEdit.href = newURL;
-            }
-        </script>
     </body>
 </html>
